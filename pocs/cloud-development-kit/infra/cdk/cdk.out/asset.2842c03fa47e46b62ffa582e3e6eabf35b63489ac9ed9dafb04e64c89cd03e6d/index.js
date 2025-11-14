dc17f1e8054b969ab9e16e3d48d950d198247d5c
leonardosalles <leosallestm@gmail.com>
2025-11-14 03:24:41 -0300

    Remove wrong files

--- pocs/cloud-development-kit/infra/cdk/cdk.out/asset.2842c03fa47e46b62ffa582e3e6eabf35b63489ac9ed9dafb04e64c89cd03e6d/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.handler = async (event) => {
-    console.log("AppSync event", JSON.stringify(event));
-    return "Lambda Resolver";
-};
--- pocs/cloud-development-kit/infra/cdk/lib/env-config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prefix = exports.region = exports.ENV = exports.APP_NAME = void 0;
-exports.APP_NAME = process.env.APP_NAME ?? 'myapp';
-exports.ENV = process.env.ENV ?? 'dev';
-exports.region = process.env.AWS_REGION ?? 'us-east-1';
-exports.prefix = `${exports.APP_NAME}-${exports.ENV}`;
