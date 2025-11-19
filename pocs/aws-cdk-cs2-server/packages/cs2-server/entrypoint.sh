e955d5f6892c779085e572cd1fc0dfcd39fa6718
leonardosalles <leosallestm@gmail.com>
2025-11-19 11:43:02 -0300

    Fix directory and add steamcmd

--- pocs/aws-cdk-cs2-server/packages/cs2-server/entrypoint.sh
+++ pocs/aws-cdk-cs2-server/packages/cs2-server/entrypoint.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SRCDS_DIR=/home/steam/cs2/game
+SRCDS_DIR=/home/steam/cs2
 
-echo "Starting Watercooler Server..."
+echo "[CS2] Ensuring install directory exists..."
+mkdir -p "${SRCDS_DIR}"
 
-exec "${SRCDS_DIR}/bin/linuxsteamrt64/cs2" \
+echo "[CS2] Updating server..."
+/home/steam/steamcmd/steamcmd.sh \
+  +force_install_dir ${SRCDS_DIR} \
+  +login anonymous \
+  +app_update 730 validate \
+  +quit
+
+echo "[CS2] Update complete."
+
+echo "[CS2] Starting Watercooler Server..."
+
+exec ${SRCDS_DIR}/game/bin/linuxsteamrt64/cs2 \
   -dedicated \
   -insecure \
   -usercon \
