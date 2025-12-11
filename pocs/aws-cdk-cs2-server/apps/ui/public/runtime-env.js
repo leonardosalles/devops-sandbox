c60120c05022b857eea42c7d2ec0dfb24318a506
leonardosalles <leosallestm@gmail.com>
2025-12-11 01:05:32 -0300

    Fixes

--- pocs/aws-cdk-cs2-server/apps/ui/public/runtime-env.js
+++ pocs/aws-cdk-cs2-server/apps/ui/public/runtime-env.js
@@ -1,3 +1,3 @@
 window.runtimeEnv = {
-  NEXT_PUBLIC_API_URL: "https://qnfjecnl9c.execute-api.sa-east-1.amazonaws.com/prod/"
+  NEXT_PUBLIC_API_URL: "https://2hd3gy93n3.execute-api.sa-east-1.amazonaws.com/prod/"
 };
