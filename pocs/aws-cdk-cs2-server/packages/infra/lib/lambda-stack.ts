43351fc12291801ac78ab1f9a2117d95e79b2413
leonardosalles <leosallestm@gmail.com>
2025-12-11 00:32:19 -0300

    Fix lambda

--- pocs/aws-cdk-cs2-server/packages/infra/lib/lambda-stack.ts
+++ pocs/aws-cdk-cs2-server/packages/infra/lib/lambda-stack.ts
@@ -105,16 +105,10 @@ export class LambdaStack extends Stack {
 
     props.table.grantReadWriteData(this.controlFn);
 
-    const adminIdsParam = new ssm.StringParameter(this, "AdminIdsParam", {
+    new ssm.StringParameter(this, "AdminIdsParam", {
       parameterName: "/cs2/admin-ids",
       stringValue: props.steamAdminIds,
     });
-
-    adminIdsParam.grantRead(props.ec2Role);
-
-    props.ec2Role.addManagedPolicy(
-      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore")
-    );
 
     new cdk.CfnOutput(this, "Environment", {
       value: JSON.stringify(environment),
