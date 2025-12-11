498f020add12a7976cc97f3b9cdb530f63d1a394
leonardosalles <leosallestm@gmail.com>
2025-12-11 00:40:39 -0300

    Fix text

--- pocs/aws-cdk-cs2-server/apps/ui/app/components/LoadingButton.tsx
+++ pocs/aws-cdk-cs2-server/apps/ui/app/components/LoadingButton.tsx
@@ -15,7 +15,7 @@ export default function LoadingButton({
       {...props}
       disabled={loading || props.disabled}
       className={`
-        relative px-3 py-2 rounded-md text-white transition
+        relative px-3 py-2 rounded-md transition
         disabled:opacity-50 disabled:cursor-not-allowed
         ${className}
       `}
